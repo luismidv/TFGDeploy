9de8beac879e2090504e6a924c96b37169ec034f
luismidv <perezmartluismiguel@gmail.com>
2025-02-13 17:25:20 +0100

    Responsive trials

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -47,7 +47,7 @@ const HomePage = () => {
   
             <div className="relative mt-4">
               <input className="room-search w-full md:w-auto" type="search" name="q" placeholder="Where do you need a room" />
-              <img className="search-img absolute top-0 right-0 w-6 h-6" src={searchImage} alt="Search" />
+              <img className="search-img absolute w-6 h-6" src={searchImage} alt="Search" />
               <button className="submit-search" type="submit"></button>
             </div>
             <Link className="log-in-button w-full sm:w-auto mt-4" to="/login">
