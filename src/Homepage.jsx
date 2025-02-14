6ab3fe9ec6d79772f2aab0628d17a0211f0152ba
luismidv <perezmartluismiguel@gmail.com>
2025-02-14 11:11:04 +0100

    Responsive trials

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -27,21 +27,22 @@ const HomePage = () => {
         <div id="image-expo" className="image-expositor">
           <AppExpo />
         </div>
+        <div className = "text-center md:text-left relative md:translate-y-0 translate-y-[500px] transition-transform duration-300">
+          <p className="general-description text-center md:text-left">
+            Do not waste your time, we do it for you.
+          </p>
+          <p className="general-description-p text-center md:text-left">
+            Searching for house partners can be difficult, we provide an opportunity <br />
+            to get the best possible partners.Our powered by AI system gives us the power<br /> for selecting your perfect match. <br />
+            Fill out the following data so we can find your match considering your preferences.
+          </p>
   
-        <p className="general-description text-center md:text-left">
-          Do not waste your time, we do it for you.
-        </p>
-        <p className="general-description-p text-center md:text-left">
-          Searching for house partners can be difficult, we provide an opportunity <br />
-          to get the best possible partners.Our powered by AI system gives us the power<br /> for selecting your perfect match. <br />
-          Fill out the following data so we can find your match considering your preferences.
-        </p>
-  
-        <p className="general-description text-center md:text-left" style={{ top: '1730px', left: '237px' }}>Let the hard work be for us</p>
-        <a className="form-button block w-full md:w-auto" href="./src/templates/room/started.html" target="_blank" rel="noopener noreferrer">
-          Get started
-        </a>
-        <img className="room-idx w-full md:w-auto" src={roomIdxImage} alt="Room Index" />
+          <p className="general-description text-center md:text-left" style={{ top: '1730px', left: '237px' }}>Let the hard work be for us</p>
+          <a className="form-button block w-full md:w-auto" href="./src/templates/room/started.html" target="_blank" rel="noopener noreferrer">
+            Get started
+          </a>
+          <img className="room-idx w-full md:w-auto" src={roomIdxImage} alt="Room Index" />
+        </div>
   
         <section className="why-us text-center md:text-lef relative md:translate-y-0 translate-y-[500px] transition-transform duration-300">
           <h1 className="why-us-h1">Why choosing Roomatch</h1>
